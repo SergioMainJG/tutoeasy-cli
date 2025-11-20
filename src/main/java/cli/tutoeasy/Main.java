f7bdec4e470dc0adc2ca4884bb0ff77f6cbd0808
SergioMainJG <arce.roldan.sergio@gmail.com>
2025-11-20 07:24:20 -0600

    add: command/ for picocli's implementation; config/EnvConfig.java and config/JPAUtil.java for global config of MySQL connection and envs; model/ for models to manage them in the application and  be inserted in db; repository/ for the management of querys into MySQL; service/ for specific validations and call domain logic; util/ for global validations, global helpers, etc

--- src/main/java/cli/tutoeasy/Main.java
+++ src/main/java/cli/tutoeasy/Main.java
@@ -1,17 +1,29 @@
 package cli.tutoeasy;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import jakarta.persistence.EntityManager;
+import cli.tutoeasy.config.JPAUtil;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        System.out.println("Intentando iniciar la conexión JPA/Hibernate...");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        EntityManager em = null;
+        try {
+            em = JPAUtil.getEntityManager();
+
+            System.out.println("Conexión exitosa a la base de datos MySQL.");
+
+
+        } catch (Exception e) {
+            System.err.println("Fallo en la conexión o inicialización de Hibernate.");
+            e.printStackTrace();
+        } finally {
+            if (em != null && em.isOpen()) {
+                em.close();
+            }
+            JPAUtil.shutdown();
         }
+
+        System.out.println("Aplicación terminada.");
     }
 }
