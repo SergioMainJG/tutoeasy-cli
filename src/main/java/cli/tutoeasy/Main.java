fa907f29240c3e753fb3561add07fb31d9310263
Daniel Duran <daniel.duran0725@jala.university>
2025-11-24 21:21:53 -0300

    feat: Student registration
    # Conflicts:
    #	src/main/java/cli/tutoeasy/Main.java

--- src/main/java/cli/tutoeasy/Main.java
+++ src/main/java/cli/tutoeasy/Main.java
@@ -3,9 +3,11 @@ package cli.tutoeasy;
 import cli.tutoeasy.command.AdminCommand;
 import cli.tutoeasy.command.LoginCommand;
 import cli.tutoeasy.command.RootCommand;
+import cli.tutoeasy.command.StudentCommand;
 import cli.tutoeasy.repository.UserRepository;
 import cli.tutoeasy.service.AdministratorService;
 import cli.tutoeasy.service.AuthService;
+import cli.tutoeasy.service.StudentService;
 import picocli.CommandLine;
 
 public class Main {
@@ -15,12 +17,16 @@ public class Main {
                 ClassLoader.getSystemResource("logging.properties").getPath());
 
         UserRepository userRepo = new UserRepository();
+        AuthService authService = new AuthService(userRepo);
+        StudentService studentService = new StudentService(userRepo);
        AuthService authService = new AuthService(userRepo);
         AdministratorService adminService = new AdministratorService(userRepo);
 
-       new CommandLine(new RootCommand())
-               .addSubcommand("Login", new LoginCommand(authService))
-               .addSubcommand("create-admin", new AdminCommand(adminService))
-               .execute(args);
+        new CommandLine(new RootCommand())
+                .addSubcommand("Login", new LoginCommand(authService))
+                .addSubcommand("Create-student", new StudentCommand(studentService))
+                .addSubcommand("Login", new LoginCommand(authService))
+                .addSubcommand("create-admin", new AdminCommand(adminService))
+                .execute(args);
     }
 }
