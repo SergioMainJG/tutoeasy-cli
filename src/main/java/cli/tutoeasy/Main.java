484c76a20ef5514ffbab6b4b751b37d9fdbec6a4
Daniel Duran <daniel.duran0725@jala.university>
2025-11-26 21:57:52 -0300

    feat: Create Tutor command

--- src/main/java/cli/tutoeasy/Main.java
+++ src/main/java/cli/tutoeasy/Main.java
@@ -4,10 +4,12 @@ import cli.tutoeasy.command.AdminCommand;
 import cli.tutoeasy.command.LoginCommand;
 import cli.tutoeasy.command.RootCommand;
 import cli.tutoeasy.command.StudentCommand;
+import cli.tutoeasy.command.TutorCommand;
 import cli.tutoeasy.repository.UserRepository;
 import cli.tutoeasy.service.AdministratorService;
 import cli.tutoeasy.service.AuthService;
 import cli.tutoeasy.service.StudentService;
+import cli.tutoeasy.service.TutorService;
 import picocli.CommandLine;
 
 public class Main {
@@ -19,14 +21,14 @@ public class Main {
         UserRepository userRepo = new UserRepository();
         AuthService authService = new AuthService(userRepo);
         StudentService studentService = new StudentService(userRepo);
-        AuthService authService = new AuthService(userRepo);
+        TutorService tutorService = new TutorService(userRepo);
         AdministratorService adminService = new AdministratorService(userRepo);
 
         new CommandLine(new RootCommand())
                 .addSubcommand("Login", new LoginCommand(authService))
                 .addSubcommand("Create-student", new StudentCommand(studentService))
-                .addSubcommand("Login", new LoginCommand(authService))
-                .addSubcommand("create-admin", new AdminCommand(adminService))
+                .addSubcommand("Create-tutor", new TutorCommand(tutorService))
+                .addSubcommand("Create-admin", new AdminCommand(adminService))
                 .execute(args);
     }
 }
